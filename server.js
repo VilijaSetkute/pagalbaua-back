a2f6352bf6e1b6605f4ded62c87e9c955034e3c8
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-04-27 12:31:58 +0300

    cors updated

--- server.js
+++ server.js
@@ -9,28 +9,28 @@ require('dotenv/config')
 const mainRouter = require('./routes/mainRoute')
 
 
-// app.use((req, res, next) => {
-//     // Website you wish to allow to connect
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//     // Request methods you wish to allow
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-//     // Request headers you wish to allow
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "X-Requested-With,content-type"
-//     );
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(express.json())
-const origin = "http://localhost:3000"
-app.use(cors({credentials: true, origin: origin}))
+// const origin = "http://localhost:3000"
+// app.use(cors({credentials: true, origin: origin}))
 app.use(bodyParser.json())
 app.use('/', mainRouter)
 

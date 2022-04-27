36caa8708e89251a582affc07d96913b4af6b6fa
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-04-27 11:47:16 +0300

    server updated

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
+
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv/config')
-const app = express()
-const mainRouter = require('./routes/mainRoute')
-const cors = require('cors')
 
-app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    // Request methods you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    // Request headers you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
+const mainRouter = require('./routes/mainRoute')
+
+
+// app.use((req, res, next) => {
+//     // Website you wish to allow to connect
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     // Request methods you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     // Request headers you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "X-Requested-With,content-type"
+//     );
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     next();
+// });
 
 app.use(express.json())
-// app.use(cors())
+const origin = "http://localhost:3000"
+app.use(cors({credentials: true, origin: origin}))
 app.use(bodyParser.json())
 app.use('/', mainRouter)
 
@@ -38,6 +42,5 @@ mongoose.connect(process.env.MONGODB_KEY)
         console.log(e)
         console.log('failed to connect')
     })
-
 
 app.listen(4000)

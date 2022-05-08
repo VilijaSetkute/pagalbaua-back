5c9d436c405caa0ac6f0efb33412fd0f633b62df
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-05-08 19:45:24 +0300

    server.js update

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json())
+const origin = "http://localhost:3000"
 app.use(cors({credentials: true, origin: origin}))
 app.use(bodyParser.json())
 app.use('/', mainRouter)
